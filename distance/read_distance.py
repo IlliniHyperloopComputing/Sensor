89101fd0201ee5fab1842d99956e5ebff3efa7da
Badtoasters <curtislowder@gmail.com>
2016-09-25 12:47:18 -0500

    Update to read_distance.py

--- distance/read_distance.py
+++ distance/read_distance.py
@@ -15,12 +15,13 @@ adc = ADS1115_Read()
 
 count = 0;
 while count < 20000:
-  distance = adc.read(3)
+  distance = adc.read(0)
   distance = 12.434 * ( distance ** (-1.065))#approximation into cm
-  xaccel = adc.read(0)#accelerometer also hooked up
-  yaccel = adc.read(1)
-  zaccel = adc.read(2)
-  print "Dist: %.4f\tx: %.4f\ty: %.4f\tz: %.4f" % (distance,xaccel,yaccel,zaccel)
+#  xaccel = adc.read(0)#accelerometer also hooked up
+#  yaccel = adc.read(1)
+#  zaccel = adc.read(2)
+
+  print "Dist: %.4f" % (distance)
   count += 1
 
 
